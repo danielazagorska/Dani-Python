3083d9e8445300c61b189751050a56ba938b7227
daniela.zagorska <daniela.zagorska@gmail.com>
2015-07-14 23:29:51 +0300

    chaper 9

---
+++ problems/9.3_Histogram.py
@@ -0,0 +1,17 @@
+__author__ = 'daniela.zagorska'
+fname = raw_input('Enter the file name: ')
+try:
+    fhand = open(r"C:\Users\daniela.zagorska\Documents\PycharmProjects\Dani-Python\problems\\" + fname)
+except:
+   exit()
+counts = dict()
+for line in fhand:
+     if len(line)>0:
+        words = line.split(' ')
+        if words[0]== 'From':
+            if words[1] in counts:
+                counts[words[1]] += 1
+            else:
+                counts[words[1]] = 1
+print counts
+
